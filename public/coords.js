5d78ad81891062a90eae25489acb84e9388a03a5
Renny <rennydan72@gmail.com>
2023-06-05 08:16:32 +0530

    Update fetch

--- public/coords.js
+++ public/coords.js
@@ -6,9 +6,14 @@ if ('geolocation' in navigator) {
         const lon = position.coords.longitude;
         const options = {
             method: 'POST',
+            mode: "cors", 
+            cache: "no-cache",
+            credentials: "same-origin", 
             headers: {
                 "Content-Type": "application/json",
             },
+            redirect: "follow", 
+            referrerPolicy: "no-referrer", 
             body: JSON.stringify({lat, lon}),
         }
 
